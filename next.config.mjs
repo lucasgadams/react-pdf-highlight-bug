b39390ab9b0586064b6071dd295d31b26293746f
Lucas Adams <lucasgreeneadams@gmail.com>
2022-11-07 14:38:13 -0500

    enabled highlighting bug

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true, // With strict mode on the highlight bug repros. When off the bug is fixed.
 };
 
 export default nextConfig;
